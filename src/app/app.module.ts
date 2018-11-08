612f0f41153acf99b44f1158a6d22494c2dd1e4b
booellean <booelle.commissions@gmail.com>
2018-11-07 19:55:43 -0800

    initial commit: includes generated componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { IllustrationComponent } from './illustration/illustration.component';
+import { WebdevComponent } from './webdev/webdev.component';
+import { DesignComponent } from './design/design.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    IllustrationComponent,
+    WebdevComponent,
+    DesignComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
