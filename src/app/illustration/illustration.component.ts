17d2504a8a8276950f0b971b14797e144a99961a
booellean <booelle.commissions@gmail.com>
2018-11-09 16:15:47 -0800

    feat: Previews of Media and Behaviors added to WebDev, Illustration, and Design components

--- src/app/illustration/illustration.component.ts
+++ src/app/illustration/illustration.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { DeferLoadModule } from '@trademe/ng-defer-load';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-illustration',
   templateUrl: './illustration.component.html',
-  styleUrls: ['./illustration.component.scss']
+  styleUrls: ['./illustration.component.scss'],
+  animations:[
+    trigger('preview-container',[
+      transition('* => *',[
+        query(':enter', style({opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('50ms', [
+          animate('.1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(10%)', offset: 0}),
+            style({opacity: 1, transform: 'translateY(0%)', offset: 1}),
+          ]))
+        ]), { delay: 1200 }) //end :leave query
+      ])  //end transition
+    ])]//end trigger //end animation
 })
 export class IllustrationComponent implements OnInit {
+
+  imagePath = '../../assets/images/illustration/';
 
   doodles =[{
     name: 'doodle-old-lady',
@@ -16,7 +30,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.pablopicasso.org/blue-period.jsp',
     title: 'Doodle of an Elderly Woman',
     description: `A study of an elderly woman's face, this was in response to the fear of aging and death after a series of severe panic attacks. Part of the purpose of this study was to find a peace in absence, the void, or whatever it is that follows us after we die. The somewhat relaxed strain on the figure's face is reminiscent of the face my grandmother held after her passing. The colorful strokes that meld into her hair spiral and wave into unknown territory. All of this while the colors contrast one another in warmth and coolness`,
-    date: 20161107
+    date: 20161107,
+    show: false
   },{
     name: 'doodle-sans',
     alt: `A piece of fanart of the character "Sans" from the video game Undertale`,
@@ -24,7 +39,8 @@ export class IllustrationComponent implements OnInit {
     link: 'http://undertale.wikia.com/wiki/Sans',
     title: 'Doodle of Sans - FanArt',
     description: `One of my rare pieces of fanart, this is a doodle of the character "Sans" from the video game Undertale. Sans is a goofy, cartooney skeleton who cracks jokes. Because he has a permanent smile on his face, I wanted to create a realistic skill that would have that permanent smiling structure. The golden colors contrast with his cool blue sweatshirt.`,
-    date: 20160707
+    date: 20160707,
+    show: false
   },{
     name: 'doodle-fairy',
     alt: `A doodle of a Wormwood Fairy, with Wormwood leaves for wings`,
@@ -32,7 +48,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.instagram.com/p/BXjPNyJl-1N/?hl=en&taken-by=boobitters',
     title: 'WormWood Fairy',
     description: `A doodle based on a friend's idea for a character. This fairy is fond of Wormwood drinks such as absinthe and even has Wormwood leaves for wings. This pencil sketch incorporates some of the culture of said friend, her sharp smirk and punkish hairstyle.`,
-    date: 20170807
+    date: 20170807,
+    show: false
   },{
     name: 'doodle-heart',
     alt: `A doodle of a decrepit figure with a heart beating out of its chest, uncontained`,
@@ -40,7 +57,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://en.wikipedia.org/wiki/Heart',
     title: 'Doodle of Heart',
     description: `A doodle based on intense emotion, this sketch was recently after a break-up which had recently followed the death of a friend. The zombie-ish style of the figure is representative of the wear and tear emotional distress can cause on a body, despite it not always being apparent. Quite literally, the heart can not be contained despite the strain of the figure.`,
-    date: 20170507
+    date: 20170507,
+    show: false
   }];
 
   inks = [{
@@ -50,7 +68,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.instagram.com/p/BU6EOJjFCqS/?hl=en&taken-by=boobitters',
     title: 'Dream Study',
     description: `This was a study of brush mechanics, primarily a contrast of brush texture, dry brush versus wet brush. One of my favorite pieces of subject matter is the duality of expression versus internal emotion. Being a person who struggles with reading people's faces, one thing that never seems to elude me is the forced, eerie smiles of people suffering from depression.`,
-    date: 20170707
+    date: 20170707,
+    show: false
   },{
     name: 'bw-crow-study',
     alt: `Crow Study Two in brush and ink`,
@@ -58,7 +77,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.instagram.com/p/BTNzxaUlYMk/?hl=en&taken-by=boobitters',
     title: 'Crow Study Two',
     description: `A casual study of a crow, taken from a photograph. This study is less surreal than my other sketches and was inked to showcase the structure and texture of a real crow. Crows are one of my favorite subjects to sketch from because unlike other animals they tend to study you back.`,
-    date: 20170126
+    date: 20170126,
+    show: false
   },{
     name: 'bw-crow-invert-study',
     alt: `Crow Study One in brush and ink`,
@@ -66,7 +86,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.instagram.com/p/BWQLdISF_DN/?hl=en&taken-by=boobitters',
     title: 'Crow Study One',
     description: `A casual study of a crow, loosely based on memory. This study is very surreal and was more of a study on how the ink moved with the paper. After scanning this piece, I inverted the colors using a filter, which created an interesting effect with the brush texture. Crows are one of my favorite subjects to sketch from because unlike other animals they tend to study you back.`,
-    date: 20170707
+    date: 20170707,
+    show: false
   },{
     name: 'bw-sprite',
     alt: `Study of brush mechanics versus pen mechanics - baby with hair`,
@@ -74,7 +95,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.instagram.com/p/BWQLdISF_DN/?hl=en&taken-by=boobitters',
     title: 'Baby with Silky Hair',
     description: `This was a study of brush mechanics versus pen mechanics, mixing the thin stiff lines of common pens with the wet marks of an ink brush. This study started with simple shapes with no real form. As the shapes twirled inward, the form of an infant became the most apparent shape and I inked it accordingly.`,
-    date: 20170707
+    date: 20170707,
+    show: false
   }];
 
   colorPencil = [{
@@ -84,7 +106,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.linkedin.com/in/stevenjpope/',
     title: 'Wasting Time Album Cover - Version One',
     description: `A part of the "Wasting Time" album, this cover is made to wrap around the cd case so the arm spills the sands of time upwards and through his hands. The sands can be read as both a metaphor for time or a literal representation of the struggles of cocaine and similar substances. This version of the cover is yellow with minimal highlights, the paper made to emulate the rough florescent lights of a hospital room.`,
-    date: 20170120
+    date: 20170120,
+    show: false
  },{
   name: 'album-color-2',
   alt: `Album Cover "Wasting Time" by Steven Pope. Brown version`,
@@ -92,7 +115,8 @@ export class IllustrationComponent implements OnInit {
   link: 'https://www.linkedin.com/in/stevenjpope/',
   title: 'Wasting Time Album Cover - Version Two',
   description: `A part of the "Wasting Time" album, this cover is made to wrap around the cd case so the arm spills the sands of time upwards and through his hands. The sands can be read as both a metaphor for time or a literal representation of the struggles of cocaine and similar substances. This version of the cover is brown to emulate a dirtier environment than the prior version. This paper also allows for a lot more color contrast and increased depth.`,
-  date: 20170120
+  date: 20170120,
+  show: false
 },{
   name: 'album-color-3',
   alt: `Album Cover "Wasting Time" by Steven Pope. Black version`,
@@ -100,7 +124,8 @@ export class IllustrationComponent implements OnInit {
   link: 'https://www.linkedin.com/in/stevenjpope/',
   title: 'Wasting Time Album Cover - Version Three',
   description: `A part of the "Wasting Time" album, this cover is made to wrap around the cd case so the arm spills the sands of time upwards and through his hands. The sands can be read as both a metaphor for time or a literal representation of the struggles of cocaine and similar substances. This version of the cover is black to emulate a a hollowness in drug use. Unlike the previous versions, the contrast is stark and the highlights on the form are the primary focus. Unlike before, the white on black allows the figure to meld into the sands of time, his matter emptying alongside it.`,
-  date: 20170120
+  date: 20170120,
+  show: false
 }];
 
   characterStudy = [{
@@ -110,7 +135,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.cdc.gov/tuskegee/timeline.htm',
     title: 'Colors - Claire Newman',
     description: `A character study for the video game Colors: The Tuskegee Studies. This is a piece of historical fiction, that takes a "clear" look into the sinister side of America's history. Main character Claire Newman is a slacktivist who, through a combination of spite for her father and relative ignorance of the world, decides to get a degree in nursing and moves to Tuskegee, Alabama after being kicked out. However, something is going on in Tuskegee that's not quite right. And Claire's addiction to substances is causing a strange reaction with the locals.`,
-    date: 20161220
+    date: 20161220,
+    show: false
   },{
     name: 'colors-clementine',
     alt: `Character Study of game character Clementine Turner, a middle-aged white woman who has a big heart under a snide smile.`,
@@ -118,7 +144,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.cdc.gov/tuskegee/timeline.htm',
     title: 'Colors - Clementine Turner',
     description: `A character study for the video game Colors: The Tuskegee Studies. This is a piece of historical fiction, that takes a "clear" look into the sinister side of America's history. Support character Clementine Turner takes in Claire Newman to spite her brother, Claire's father. A hermit who refuses to interact with anyone, Clementine has her own secrets that she'd rather not share. But first, what's going on in Tuskegee?`,
-    date: 20161220
+    date: 20161220,
+    show: false
   },{
     name: 'colors-eunice-rivers',
     alt: `Character Study of game character Eunice Rivers, an elderly black nurse who harvors a terrible secret.`,
@@ -126,7 +153,8 @@ export class IllustrationComponent implements OnInit {
     link: 'https://www.cdc.gov/tuskegee/timeline.htm',
     title: 'Colors - Eunice Rivers',
     description: `A character study for the video game Colors: The Tuskegee Studies. This is a piece of historical fiction, that takes a "clear" look into the sinister side of America's history. Based on a real person of the same name, Eunice Rivers took part in a horrible "study" by the CDC. The biggest question about Eunice; why did she do it and what for?`,
-    date: 20161220
+    date: 20161220,
+    show: false
   },{
     name: 'doodle-bhictoria-bhickie-darlene-beattie',
     alt: `Character Study of expressions on faces, character Bhickie is a fiesty person`,
@@ -134,7 +162,8 @@ export class IllustrationComponent implements OnInit {
     link: 'none',
     title: 'My oh M.I. - Bhickie Darlene Beattie',
     description: `A character study for a now debunk comic, Bhictoria, or Bhickie for short, is an emotional, proud person who has a bit of problem fitting in. My oh M.I. is a critical look at mental illness, the causes and effects of it, and how it plays into society. More importantly, My Oh M.I. asks the critical question, what constitutes a mental illness and who has the right to define that?`,
-    date: 20170220
+    date: 20170220,
+    show: false
   },{
     name: 'doodle-sage-markus-easom',
     alt: `Character Study of expressions on faces, character Markus is a passive, calm person`,
@@ -142,7 +171,8 @@ export class IllustrationComponent implements OnInit {
     link: 'none',
     title: 'My oh M.I. - Sage Markus Easom',
     description: `A character study for a now debunk comic, Sage, or better known as Markus, is a quite and timid person whose generosity is often taken for granted. My oh M.I. is a critical look at mental illness, the causes and effects of it, and how it plays into society. More importantly, My Oh M.I. asks the critical question, what constitutes a mental illness and who has the right to define that?`,
-    date: 20170220
+    date: 20170220,
+    show: false
   }];
 
   constructor() { }
--- src/app/webdev/webdev.component.ts
+++ src/app/webdev/webdev.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { DeferLoadModule } from '@trademe/ng-defer-load';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-webdev',
   templateUrl: './webdev.component.html',
-  styleUrls: ['./webdev.component.scss']
+  styleUrls: ['./webdev.component.scss'],
+  animations:[
+    trigger('preview-container',[
+      transition('* => *',[
+        query(':enter', style({opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('50ms', [
+          animate('.1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(10%)', offset: 0}),
+            style({opacity: 1, transform: 'translateY(0%)', offset: 1}),
+          ]))
+        ]), { delay: 1200 }) //end :leave query
+      ])  //end transition
+    ])]//end trigger //end animation
 })
 export class WebdevComponent implements OnInit {
+
+  imagePath = '../../assets/images/webdev/';
 
   wordpress = [{
     name: 'trefoil-cultural',
@@ -15,7 +30,8 @@ export class WebdevComponent implements OnInit {
     link: 'https://trefoilcultural.com/',
     github: 'https://github.com/booellean/TrefoilCultural',
     title: 'Trefoil Cultural and Environmental',
-    description: `This is a WordPress theme derived from the twentysixteen theme, with custom code and css. This website was commissioned to combine two of the client's former websites. This involved exporting the WordPress SQL database, creating custom categories from the old sites to control how data was dynamically generated, and transferring ownership of domain.`
+    description: `This is a WordPress theme derived from the twentysixteen theme, with custom code and css. This website was commissioned to combine two of the client's former websites. This involved exporting the WordPress SQL database, creating custom categories from the old sites to control how data was dynamically generated, and transferring ownership of domain.`,
+    show: false
    },{
      name: 'mr-ecology',
      alt: 'Website Link for Matt Rothaus Ecology',
@@ -23,7 +39,8 @@ export class WebdevComponent implements OnInit {
      link: 'http://www.mr-ecology.com',
      github: 'https://github.com/booellean/MARothaus-Blog',
      title: 'Matt Rothaus Blog',
-     description: `This is a custom WordPress website designed without a theme template. The Form is all custom PHP code. The structure of the the DOM content proved to be a difficult case for Edge and IE browsers due to a high reliance on fixed positioning. I was able to correct this by also fixing the position of all container tags and having overflow for child elements. With clever styling, I was able to style the overflow scrollbar to look natural with the browser's window scroll.`
+     description: `This is a custom WordPress website designed without a theme template. The Form is all custom PHP code. The structure of the the DOM content proved to be a difficult case for Edge and IE browsers due to a high reliance on fixed positioning. I was able to correct this by also fixing the position of all container tags and having overflow for child elements. With clever styling, I was able to style the overflow scrollbar to look natural with the browser's window scroll.`,
+     show: false
     },{
       name: 'vulpe-venture',
       alt: `Website Link for Victoria Vulpe's Blog`,
@@ -31,7 +48,8 @@ export class WebdevComponent implements OnInit {
       github: 'https://github.com/booellean/TheVulpeVenture',
       link: 'http://thevulpeventure.com',
       title: 'Victoria Vulpe Blog',
-      description: `This is a custom WordPress theme derived from the twentyseventeen theme with custom css and code. One part I am most proud of with this code was by using WordPress' custom preview images to generate wrapper backgrounds based on individual blog posts.`
+      description: `This is a custom WordPress theme derived from the twentyseventeen theme with custom css and code. One part I am most proud of with this code was by using WordPress' custom preview images to generate wrapper backgrounds based on individual blog posts.`,
+      show: false
     }];
 
   staticPages = [{
@@ -41,7 +59,8 @@ export class WebdevComponent implements OnInit {
     github: 'https://github.com/booellean/poly-entertainment',
     link: 'http://www.polyentertainment.com',
     title: 'Poly entertainment',
-    description: 'This is a static website for Poly entertainment. This website incorporates JQuery, canvas animations, hamburger menus, and custom svg graphics.'
+    description: 'This is a static website for Poly entertainment. This website incorporates JQuery, canvas animations, hamburger menus, and custom svg graphics.',
+    show: false
   },{
     name: 'map-mothers-card',
     alt: `Website link for Mother's Day Card with Map`,
@@ -49,7 +68,8 @@ export class WebdevComponent implements OnInit {
     github: 'https://github.com/booellean/map',
     link: 'https://booellean.github.io/map/',
     title: `Map Mother's Day Card`,
-    description: `This is a static website for a digital mother's day card. This card utilizes google's map API to "bring" my mother to my favorite locations in Seattle. When the map coordinates are loaded, an associated video plays with that location so my mother can see where that location is.`
+    description: `This is a static website for a digital mother's day card. This card utilizes google's map API to "bring" my mother to my favorite locations in Seattle. When the map coordinates are loaded, an associated video plays with that location so my mother can see where that location is.`,
+    show: false
     },{
     name: 'dnd-form',
     alt: `Website link for D'n'D character sheet form`,
@@ -57,7 +77,8 @@ export class WebdevComponent implements OnInit {
     github: 'https://github.com/booellean/database-handlebars',
     link: 'https://booellean.github.io/database-handlebars/',
     title: `Character Form with Handlebars and AJAX`,
-    description: `This is a static website that utilizes a heroku server to save user inputted data. This form has a JQuery AJAX call to post and get data, and then generates that data into a grid table using Handlebars.`
+    description: `This is a static website that utilizes a heroku server to save user inputted data. This form has a JQuery AJAX call to post and get data, and then generates that data into a grid table using Handlebars.`,
+    show: false
     },{
     name: '8-ball',
     alt: `Website link for a sarcastic Magic 8-ball`,
@@ -65,7 +86,8 @@ export class WebdevComponent implements OnInit {
     github: 'https://github.com/booellean/8-ball',
     link: 'https://booellean.github.io/8-ball/',
     title: `Magic 8-ball with Integrated UX`,
-    description: `This is a static website that utilizes a user's microphone and gyroscope to create a unique user experiece where they can ask the ball a question and then "shake" for an answer. Some answers are pre-determined based on the question asked, despite if it's asked in an input field or microphone.`
+    description: `This is a static website that utilizes a user's microphone and gyroscope to create a unique user experiece where they can ask the ball a question and then "shake" for an answer. Some answers are pre-determined based on the question asked, despite if it's asked in an input field or microphone.`,
+    show: false
     }];
 
   constructor() { }
