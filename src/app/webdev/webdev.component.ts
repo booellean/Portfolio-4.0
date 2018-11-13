5241e768ed1e69f8d446b31cbb8ed103edaf8109
booellean <booelle.commissions@gmail.com>
2018-11-12 21:37:01 -0800

    chore: added dates to sites json

--- src/app/webdev/webdev.component.ts
+++ src/app/webdev/webdev.component.ts
@@ -24,6 +24,15 @@ export class WebdevComponent implements OnInit {
   imagePath = '../../assets/images/webdev/';
 
   wordpress = [{
+    name: 'thinkclink',
+    alt: 'Website Link for Thinkclink.com, Smart Investing',
+    fileType: '.jpg',
+    link: 'https://thinkclink.com/',
+    title: 'ThinkClink Smart Investing',
+    description: `This is a WordPress theme derived from the twentysixteen theme, with custom code and css. This website was commissioned to combine two of the client's former websites. This involved exporting the WordPress SQL database, creating custom categories from the old sites to control how data was dynamically generated, and transferring ownership of domain.`,
+    date: 1542672000,
+    show: false
+   },{
     name: 'trefoil-cultural',
     alt: 'Website Link for Trefoil Cultural and Environmental',
     fileType: '.jpg',
@@ -31,6 +40,7 @@ export class WebdevComponent implements OnInit {
     github: 'https://github.com/booellean/TrefoilCultural',
     title: 'Trefoil Cultural and Environmental',
     description: `This is a WordPress theme derived from the twentysixteen theme, with custom code and css. This website was commissioned to combine two of the client's former websites. This involved exporting the WordPress SQL database, creating custom categories from the old sites to control how data was dynamically generated, and transferring ownership of domain.`,
+    date: 1517443200,
     show: false
    },{
      name: 'mr-ecology',
@@ -40,6 +50,7 @@ export class WebdevComponent implements OnInit {
      github: 'https://github.com/booellean/MARothaus-Blog',
      title: 'Matt Rothaus Blog',
      description: `This is a custom WordPress website designed without a theme template. The Form is all custom PHP code. The structure of the the DOM content proved to be a difficult case for Edge and IE browsers due to a high reliance on fixed positioning. I was able to correct this by also fixing the position of all container tags and having overflow for child elements. With clever styling, I was able to style the overflow scrollbar to look natural with the browser's window scroll.`,
+     date: 1505606400,
      show: false
     },{
       name: 'vulpe-venture',
@@ -49,6 +60,7 @@ export class WebdevComponent implements OnInit {
       link: 'http://thevulpeventure.com',
       title: 'Victoria Vulpe Blog',
       description: `This is a custom WordPress theme derived from the twentyseventeen theme with custom css and code. One part I am most proud of with this code was by using WordPress' custom preview images to generate wrapper backgrounds based on individual blog posts.`,
+      date: 1508025600,
       show: false
     }];
 
@@ -60,6 +72,7 @@ export class WebdevComponent implements OnInit {
     link: 'http://www.polyentertainment.com',
     title: 'Poly entertainment',
     description: 'This is a static website for Poly entertainment. This website incorporates JQuery, canvas animations, hamburger menus, and custom svg graphics.',
+    date: 1549584000,
     show: false
   },{
     name: 'map-mothers-card',
@@ -69,6 +82,7 @@ export class WebdevComponent implements OnInit {
     link: 'https://booellean.github.io/map/',
     title: `Map Mother's Day Card`,
     description: `This is a static website for a digital mother's day card. This card utilizes google's map API to "bring" my mother to my favorite locations in Seattle. When the map coordinates are loaded, an associated video plays with that location so my mother can see where that location is.`,
+    date: 1494720000,
     show: false
     },{
     name: 'dnd-form',
@@ -78,6 +92,7 @@ export class WebdevComponent implements OnInit {
     link: 'https://booellean.github.io/database-handlebars/',
     title: `Character Form with Handlebars and AJAX`,
     description: `This is a static website that utilizes a heroku server to save user inputted data. This form has a JQuery AJAX call to post and get data, and then generates that data into a grid table using Handlebars.`,
+    date: 1496361600,
     show: false
     },{
     name: '8-ball',
@@ -87,6 +102,7 @@ export class WebdevComponent implements OnInit {
     link: 'https://booellean.github.io/8-ball/',
     title: `Magic 8-ball with Integrated UX`,
     description: `This is a static website that utilizes a user's microphone and gyroscope to create a unique user experiece where they can ask the ball a question and then "shake" for an answer. Some answers are pre-determined based on the question asked, despite if it's asked in an input field or microphone.`,
+    date: 1489536000,
     show: false
     }];
 
